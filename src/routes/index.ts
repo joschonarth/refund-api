19e64289292cf062e7f6903fab40b68e0622a87b
joschonarth <joschonarth@gmail.com>
2025-03-29 17:08:18 -0300

    feat: implement refunds routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,16 @@ import { Router } from 'express'
 
 import { usersRoutes } from './users-routes'
 import { sessionsRoutes } from './sessions-routes'
+import { refundsRoutes } from './refunds-routes'
+
+import { ensureAuthenticated } from '@/middlewares/ensure-authenticated'
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/sessions', sessionsRoutes)
+
+routes.use(ensureAuthenticated)
+routes.use('/refunds', refundsRoutes)
 
 export { routes }
