3ee67f6995de5ec554bc0da62ed516a093c02470
joschonarth <joschonarth@gmail.com>
2025-03-29 17:50:05 -0300

    feat: implement refund listing

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -43,6 +43,28 @@ class RefundsController {
 
     response.status(201).json(refund)
   }
+
+  async index(request: Request, response: Response) {
+    const querySchema = z.object({
+      name: z.string().optional().default(''),
+    })
+
+    const { name } = querySchema.parse(request.query)
+
+    const refunds = await prisma.refunds.findMany({
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          },
+        },
+      },
+      orderBy: { createdAt: 'desc' },
+      include: { user: true },
+    })
+
+    response.json(refunds)
+  }
 }
 
 export { RefundsController }
